80dbde03b109680e8020c474f6df5a4f2cc9ab0b
Karen Tran <karentran28@hotmail.com>
2026-02-26 13:59:12 -0800

    added middleware and login res handlers

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,22 @@ from app.database.db import engine, Base
 from app.routes.auth import router as auth_router
 from app.routes.trades import router as trades_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(trades_router)
 Base.metadata.create_all(bind = engine)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
     return {"message": "Backend is running"}
+
